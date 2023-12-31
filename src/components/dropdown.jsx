e49d4d69a2c50f63bbbb296b3e584d97a54c6542
Andre van de Ven <andrebvandeven@gmail.com>
2023-12-31 16:11:33 -0500

    reverted changes

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,26 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "./dropdown.css";
+import "./dropdown.css"
 
 const DropdownMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleDropdown = () => setIsOpen(!isOpen);
-    const closeDropdown = () => setIsOpen(false);
-
     return (
         <div className="dropdown-container">
-            {/* Toggle Dropdown */}
-            <button className="dropdown-button" onClick={toggleDropdown}>Menu</button>
-
-            {/* Dropdown Content */}
-            {isOpen && (
-                <div className="dropdown-content">
-                    <Link to="/pictures" onClick={closeDropdown}>Pictures</Link>
-                    <a href="https://github.com/andrevandeven" target="_blank" rel="noopener noreferrer" onClick={closeDropdown}>Github</a>
-                    <Link to="/about" onClick={closeDropdown}>About</Link>
-                </div>
-            )}
+            <Link to="/" className="dropdown-button">Menu</Link>
+            <div className="dropdown-content">
+                <Link to="/pictures">Pictures</Link>
+                <a href="https://github.com/andrevandeven" target="_blank" rel="noopener noreferrer">Github</a>
+                <Link to="/about">About</Link>
+            </div>
         </div>
     );
 };
